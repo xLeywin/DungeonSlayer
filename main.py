bfd240506597ca6261d69954b225ca66545a6b95
Wendell Ylan Viani <wendellylan@gmail.com>
2026-02-22 21:01:25 -0300

    Started to implement UI

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
-from PySide6.QtWidgets import QApplication
-from PySide6.QtCore import Qt, QFile
+from PySide6.QtWidgets import QApplication, QWidget
+from PySide6.QtCore import QFile
 from PySide6.QtUiTools import QUiLoader
 from PySide6.QtGui import QIcon
 
@@ -9,20 +9,37 @@ if __name__ == "__main__":
     app = QApplication(sys.argv)
     app.setStyle("Fusion")
 
-    main_ui_file = QFile("ui/main_menu.ui")
-    main_ui_file.open(QFile.ReadOnly)
+    ui_file = QFile("ui/game_window.ui")
+    ui_file.open(QFile.ReadOnly)
 
     loader = QUiLoader()
-    window = loader.load(main_ui_file)
-    main_ui_file.close()
+    window = loader.load(ui_file)
+    ui_file.close()
 
     window.setWindowIcon(QIcon("assets/icon.png"))
     window.setFixedSize(400, 520)
 
-    # Set style
     with open("styles/dark_theme.css", "r") as f:
-        style = f.read()
-        window.setStyleSheet(style)
+        window.setStyleSheet(f.read())
+
+    # get the stackedWidget
+    stack = window.findChild(QWidget, "stackedWidget")
+
+    # character page buttons
+    # confirm_button = window.findChild(QWidget, "ConfirmButton")
+
+    # Menu
+    start_button = window.findChild(QWidget, "StartButton")
+    exit_button = window.findChild(QWidget, "ExitButton")
+
+    start_button.clicked.connect(lambda: stack.setCurrentIndex(1)) # go to character edit screen (1)
+    exit_button.clicked.connect(app.quit)
+
+    # Character Edit
+    # confirm_button.clicked.connect(lambda: stack.setCurrentIndex(2))
+    
+    back_button = window.findChild(QWidget, "BackButton")
+    back_button.clicked.connect(lambda: stack.setCurrentIndex(0))
 
     window.show()
     app.exec()
