f58f0892a6fd29593edfdc553594bbbe6d7cf40d
Wendell Viani <wendellylan@gmail.com>
2026-03-01 18:01:05 -0300

    Finished character edit UI

--- main.py
+++ main.py
@@ -2,12 +2,20 @@ import sys
 from PySide6.QtWidgets import QApplication, QWidget
 from PySide6.QtCore import QFile
 from PySide6.QtUiTools import QUiLoader
-from PySide6.QtGui import QIcon
+from PySide6.QtGui import QFont, QFontDatabase, QIcon
 
 if __name__ == "__main__":
 
     app = QApplication(sys.argv)
     app.setStyle("Fusion")
+
+    font_path = "styles/PressStart2P-Regular.ttf"
+    font_id = QFontDatabase.addApplicationFont(font_path)
+    if font_id != -1:
+        family = QFontDatabase.applicationFontFamilies(font_id)[0]
+        app.setFont(QFont(family, 8)) # Define as default font
+    else:
+        print("Erro: Arquivo de fonte não encontrado.")
 
     ui_file = QFile("ui/game_window.ui")
     ui_file.open(QFile.ReadOnly)
